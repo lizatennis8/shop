9b0f6f92446e108ac9bc7170c154d0dec48d30bf
Liza Tennis and Talia Haller <talia@>
2018-08-08 15:43:59 -0700

    link commission detail input

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,20 @@
 $(document).ready(function() {
-  var size = $("#size").val();
-  var price = $("#style").val();
-  var price = $("input:radio[name=price]:checked").val();
-  var date = $("#date").val();
-  var color = $("#color").val();
 
+  $("#formOne").submit(function() {
+    var sizeInput = $("input#size").val();
+    var styleInput = $("input#style").val();
+    var priceInput = $("input:radio[name=price]:checked").val();
+    var dateInput = $("input#date").val();
+    var colorInput = $("input#color").val();
+
+  $(".size").text(sizeInput);
+  $(".style").text(styleInput);
+  $(".price").text(priceInput);
+  $(".date").text(dateInput);
+  $(".color").text(colorInput);
+
+  $("#submission").show();
+
+  event.preventDefault();
+  });
 });
