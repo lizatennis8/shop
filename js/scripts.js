436425ab433d05d90580e7a277e48fd54ecbc707
Liza Tennis and Talia Haller <talia@>
2018-08-08 15:14:11 -0700

    add commissions index

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  var size = $("#size").val();
+  var price = $("#style").val();
+  var price = $("input:radio[name=price]:checked").val();
+  var date = $("#date").val();
+  var color = $("#color").val();
+
+});
